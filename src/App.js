706479b17d2c7e6aa453cf701c829982511b07a9
Toufik353 <toufik.desai12@gmail.com>
2021-02-22 15:39:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TopBar from "./Components/TopBar/TopBar"
+import Orders from "./Components/Orders/Orders"
+
+import LoginPage from "./Components/LoginPage/LoginPage"
+import { Switch, BrowserRouter, Route, Redirect } from "react-router-dom"
+
+import ProductPage from "./Components/ProductPage/ProductPage"
+
+import UserListing from "./Components/UserListing/UserListing"
+
+
+import React, { Component } from 'react'
+
+export class App extends Component {
+  state = {
+    loginStatus: false    
+  }
+
+  handleuserLogin = () => {
+    localStorage.setItem('loginStatus', true);
+    // alert("true")
+    this.setState({ loginStatus: true })
+  }
+
+  handleuserLogout = () => {
+    localStorage.setItem('loginStatus', false);
+    // alert("false")
+    this.setState({ loginStatus: false })
+   
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <TopBar loginStatus={this.state.loginStatus} handleuserLogout={this.handleuserLogout} />
+
+
+          <Switch>
+          
+        {/* <Route exact to="/" render={(props)=>{
+          this.state.loginStatus===true?
+           <Redirect to="/productpage"/> :
+          <Redirect to="/login"/>
+        }}/> */}
+
+            {/* <Route exact path="/" render={(props)=>
+           
+            <LoginPage  handleuserLogin={this.handleuserLogin} {...props} />
+            }/> */}
+
+
+<Route exact path="/"  render={(props)=>{
+               return (
+                this.state.loginStatus===true?
+                <Redirect to="/Orders" /> :
+                <LoginPage handleuserLogin={this.handleuserLogin} {...props} />
+              )
+            }}/>
+
+
+            <Route exact path="/Products"  render={()=>{
+               return (
+                this.state.loginStatus===false?
+                <TopBar /> :
+                <ProductPage/>
+              )
+            }}/>
+
+
+<Route exact path="/Orders"  render={()=>{
+               return (
+                this.state.loginStatus==true?
+              <Orders/> :
+                <Redirect to="/Orders"/>
+              )
+            }}/> 
+
+
+
+            <Route exact path="/Orders"  render={()=>{
+               return (
+                this.state.loginStatus===false?
+                <TopBar/> :
+                <Orders/>
+              )
+            }} />
+
+
+
+            <Route exact path="/Users"  render={()=>{
+               return (
+                this.state.loginStatus===false?
+                <TopBar /> :
+                <UserListing/>
+              )
+            }} />
+          </Switch>
+        </BrowserRouter>
+{/* component={Orders} */}
+
+
+      </div>
+    )
+  }
 }
+
+
+
 
 export default App;
